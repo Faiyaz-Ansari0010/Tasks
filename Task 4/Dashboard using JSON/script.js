5882f82b754be3f39195a2578482faeb28c672ad
Faiyaz <faiyaz.ansari@zeuslearning.com>
2024-07-30 12:58:24 +0530

    Completed the entire Dashboard Page using JSON

--- Task 4/Dashboard using JSON/script.js
+++ Task 4/Dashboard using JSON/script.js
@@ -103,9 +103,111 @@ const alertInfo = [
 
 const boxInfo = [
     {
-        "logo": "imageMask1.png",
-        "alt": "image-mask-1",
+        "boxPaddingZero": false,
+        "boxTopLeftMargin": false,
+        "logoImg": "imageMask1.png",
         "heading": "Acceleration",
+        "favIconImg": "favourite.svg",
+        "favIconOpaque": false,
+        "subject": "Physics",
+        "seperator1": "|",
+        "grade": "Grade 7 +2",
+        "chapterComponents": ["Units", "Lessons", "Topics"],
+        "chapterComponentsNums": [4, 18, 24],
+        "className": "Mr. Frank's Class B",
+        "classNameOpaque": false,
+        "arrowDownImg": "arrow-down.svg",
+        "numStudents": "50 Students",
+        "seperator2": "|",
+        "duration": "21-Jan-2020 - 21-Aug-2020",
+        "preview": "preview.svg",
+        "manageCourse": "manage course.svg",
+        "gradeSubmission": "grade submission.svg",
+        "iconsOpaque": false,
+        "reports": "reports.svg",
+        "isExpired": false,
+        "statsMargin": false
+    },
+
+    {
+        "boxPaddingZero": false,
+        "boxTopLeftMargin": false,
+        "logoImg": "imageMask2.png",
+        "heading": "Displacement, Velocity and Speed",
+        "favIconImg": "favourite.svg",
+        "favIconOpaque": false,
+        "subject": "Physics",
+        "seperator1": "|",
+        "grade": "Grade 6 +3",
+        "chapterComponents": ["Units", "Lessons", "Topics"],
+        "chapterComponentsNums": [2, 15, 20],
+        "className": "No Classes",
+        "classNameOpaque": true,
+        "arrowDownImg": "arrow-down.svg",
+        "numStudents": "300 Students",
+        "seperator2": "",
+        "duration": "",
+        "preview": "preview.svg",
+        "manageCourse": "manage course.svg",
+        "gradeSubmission": "grade submission.svg",
+        "iconsOpaque": true,
+        "reports": "reports.svg",
+        "isExpired": false,
+        "statsMargin": false
+    },
+
+    {
+        "boxPaddingZero": false,
+        "boxTopLeftMargin": false,
+        "logoImg": "imageMask3.png",
+        "heading": "Introduction to Biology: Micro organisms and how they affect the other life systems in Environment",
+        "favIconImg": "favourite.svg",
+        "favIconOpaque": false,
+        "subject": "Biology",
+        "seperator1": "|",
+        "grade": "Grade 4 +1",
+        "chapterComponents": ["Units", "Lessons", "Topics"],
+        "chapterComponentsNums": [5, 16, 22],
+        "className": "All Classes",
+        "classNameOpaque": false,
+        "arrowDownImg": "arrow-down.svg",
+        "numStudents": "",
+        "seperator2": "",
+        "duration": "",
+        "preview": "preview.svg",
+        "manageCourse": "manage course.svg",
+        "gradeSubmission": "grade submission.svg",
+        "iconsOpaque": true,
+        "reports": "reports.svg",
+        "isExpired": false,
+        "statsMargin": false
+    },
+
+    {
+        "boxPaddingZero": true,
+        "boxTopLeftMargin": true,
+        "logoImg": "imageMask4.svg",
+        "heading": "Introduction to High School Mathematics",
+        "favIconImg": "favourite.svg",
+        "favIconOpaque": true,
+        "subject": "Mathematics",
+        "seperator1": "|",
+        "grade": "Grade 8 +3",
+        "chapterComponents": ["", "", ""],
+        "chapterComponentsNums": ["", "", ""],
+        "className": "Mr. Frank's Class A",
+        "classNameOpaque": false,
+        "arrowDownImg": "arrow-down.svg",
+        "numStudents": "44 Students",
+        "seperator2": "|",
+        "duration": "14-Oct-2019 - 20-Oct-2020",
+        "preview": "preview.svg",
+        "manageCourse": "manage course.svg",
+        "gradeSubmission": "grade submission.svg",
+        "iconsOpaque": false,
+        "reports": "reports.svg",
+        "isExpired": true,
+        "statsMargin": true
     }
 ]
 
@@ -179,51 +281,66 @@ document.getElementById("alert-outer-container").innerHTML = `
 
 `
 
-//     < div class="first-box-top" >
+document.getElementById("box-container").innerHTML = `
 
-//         <img class="main-image" src="imageMask1.png" alt="image-mask-1">
+    ${boxInfo.map(data => `
+        
+         <div class="box-container-card ${data.boxPaddingZero ? "box-padding-zero" : ""}">
 
-//             <div class="class-info">
+            ${data.isExpired ? `<div class="expired">EXPIRED</div>` : ""}
 
-//                 <div class="heading">
-//                     <div>Acceleration</div>
-//                     <img class="favourite-icon" src="favourite.svg" alt="favourite-icon">
-//                 </div>
+            <div class="box-container-card-top ${data.boxTopLeftMargin ? "box-top-left-margin" : ""}">
 
-//                 <div class="subject-grade">
-//                     <span>Physics</span>
-//                     <span class="seperation">|</span>
-//                     <span>Grade 7 +2</span></span>
-//             </div>
+                <img class="main-image" src="${data.logoImg}" alt="image-mask">
 
+                <div class="class-info">
 
-//             <div class="chapters">
-//                 <span class="nums">4</span>
-//                 <span class="words">Units</span>
-//                 <span class="nums">18</span>
-//                 <span class="words">Lessons</span>
-//                 <span class="nums">24</span>
-//                 <span class="words">Topics</span>
-//             </div>
+                    <div class="heading">
+                        <div>${data.heading}</div>
+                        <img class="favourite-icon ${data.favIconOpaque ? "favourite-icon-4" : ""}" src="favourite.svg" alt="favourite-icon">
+                    </div>
+                    
+                    <div class="subject-grade">
+                        <span>${data.subject}</span> 
+                        <span class="seperation">${data.seperator1}</span> 
+                        <span>${data.grade}</span></span>
+                    </div>
 
-//             <div class="class-name">
-//                 <span>Mr. Frank's Class B</span>
-//                 <img src="arrow-down.svg" alt="dropdown icon">
-//             </div>
+                    <div class="chapters">
+                        <span class="nums">${data.chapterComponentsNums[0]}</span> 
+                        <span class="words">${data.chapterComponents[0]}</span> 
+                        <span class="nums">${data.chapterComponentsNums[1]}</span>
+                        <span class="words">${data.chapterComponents[1]}</span> 
+                        <span class="nums">${data.chapterComponentsNums[2]}</span> 
+                        <span class="words">${data.chapterComponents[2]}</span>
+                    </div>
 
-//             <div class="class-detail">
-//                 <span>50 Students</span>
-//                 <span class="seperation">|</span>
-//                 <span>21-Jan-2020 - 21-Aug-2020</span>
-//             </div>
+                    <div class="class-name">
+                        <span class = "${data.classNameOpaque ? "less-opaque" : ""}">${data.className}</span>
+                        <img src="arrow-down.svg" alt="dropdown icon">
+                    </div>
 
-//         </div>
+                    <div class="class-detail">
+                        <span>${data.numStudents}</span> 
+                        <span class="seperation">${data.seperator2}</span> 
+                        <span>${data.duration}</span>
+                    </div>
 
-// </div >
+                </div>
+                
+            </div>
 
-//     <div class="class-stats">
-//         <img class="preview-icon" src="preview.svg" alt="preview">
-//             <img class="manage-course-icon" src="manage course.svg" alt="manage-course">
-//                 <img class="grade-submission-icon" src="grade submissions.svg" alt="grade-submissions">
-//                     <img class="reports-icon" src="reports.svg" alt="reports">
-//                     </div>
+            <div class="class-stats ${data.statsMargin ? "last-class-stats" : ""}">
+                <img class="preview-icon" src="preview.svg" alt="preview">
+                <img class="manage-course-icon ${data.iconsOpaque ? "less-opaque" : ""}" src="manage course.svg" alt="manage-course">
+                <img class="grade-submission-icon ${data.iconsOpaque ? "less-opaque" : ""}" src="grade submissions.svg" alt="grade-submissions">
+                <img class="reports-icon" src="reports.svg" alt="reports">
+            </div>
+
+        </div>
+
+    `).join('')
+
+    }
+
+`
