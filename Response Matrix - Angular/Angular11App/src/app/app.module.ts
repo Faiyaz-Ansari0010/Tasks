bfe85d21a3ad6f4d837b45648cfc95e87b6459a8
Faiyaz <faiyaz.ansari@zeuslearning.com>
2024-10-04 17:03:56 +0530

    Adding nav-bar, question-info and response-properties components

--- Response Matrix - Angular/Angular11App/src/app/app.module.ts
+++ Response Matrix - Angular/Angular11App/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { MatrixControlsComponent } from './Components/matrix-controls/matrix-controls.component';
+import { QuestionInfoComponent } from './Components/question-info/question-info.component';
+import { QuestionNameComponent } from './Components/question-name/question-name.component';
+import { ResponsePropertiesComponent } from './Components/response-properties/response-properties.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    NavBarComponent,
+    MatrixControlsComponent,
+    QuestionInfoComponent,
+    QuestionNameComponent,
+    ResponsePropertiesComponent,
   ],
   imports: [
     BrowserModule,
